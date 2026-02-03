c2434ae96d05a3012747c86d981acbd78527327d
sw-oo <140137784+sw-oo@users.noreply.github.com>
2026-02-03 21:40:46 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        int result = 0;
+
+        for (int i = 0; i < N ; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = N-1; i >= 0; i--) {
+            if(arr[i] > K) {
+                continue;
+            }
+            else {
+                result += K / arr[i];
+                K = K % arr[i];
+                if(K == 0) break;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
